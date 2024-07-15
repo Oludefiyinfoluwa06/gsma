9f1e375a249446fd5e27a49f9ba6ca01fed520e3
Timothy <oludefiyinfoluwa06@gmail.com>
2024-07-15 10:56:23 +0100

    update

--- server/index.js
+++ server/index.js
@@ -20,10 +20,7 @@ const port = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: {
-        origin: 'http://localhost:3000',
-        methods: ['GET', 'POST']
-    }
+    cors: {}
 });
 
 app.use(cors());
